5be38ff3927078fb811a307607bc64a7118f21bc
hex <192039@163.com>
2022-02-08 23:07:25 +0800

    UPDATE

--- app/home/views.py
+++ app/home/views.py
@@ -454,15 +454,14 @@ def search(page=None):
 @home.route('/play/<int:id>/<int:page>/',methods=['GET','POST'])
 def play(id=None,page=None):
     form = CommentForm()
-    print('1')
     movie = Movie.query.get_or_404(id)
     grade = 0
-    # if 'user' in session:
-    #     grade = Grade.query.get_or_404(session['user_id'])
-    #     print('grade1', grade)
-    # else:
-    #     grade = 0
-    #     print('grade2', grade)
+    if 'user' in session:
+        grade = Grade.query.get_or_404(session['user_id'])
+        print('grade1', grade)
+    else:
+        grade = 0
+        print('grade2', grade)
     movie.playnum = movie.playnum + 1 #点开一次,播放数+1
     tag = Tag.query.filter_by(id=movie.tag_id).first()
     #获取评论列表
@@ -477,7 +476,6 @@ def play(id=None,page=None):
         Comment.addtime.desc()
     ).paginate(page=page,per_page=6)
     #提交评论
-    print('2')
     if 'user' in session and form.validate_on_submit():
         data = form.data
         comment = Comment(
@@ -496,7 +494,6 @@ def play(id=None,page=None):
     #修改movie
     db.session.add(movie)
     db.session.commit()
-    print('3')
     return render_template('home/play.html',movie=movie, grade=grade, tag=tag,form=form,page_data=page_data)
 
 # 404页面 (去蓝图__init__.py中定义,而不是在这个视图中)
